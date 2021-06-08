7a465bb7dc1c2485e4435cab53dd5976f6c4a2eb
Luiz Zanini <luiz.kasputis@hotmail.com>
2021-06-08 17:44:31 -0300

    add sys call cfs

--- 1/initramfs/init.c
+++ 1/initramfs/init.c
@@ -14,12 +14,19 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void panic(const char *msg){
+#define len(_arr) ((int)((&_arr)[1] - _arr))
+
+
+static const char * const programs[] = { "/stack_push", "/stack_pop" };
+
+void panic(const char *msg)
+{
 	fprintf(stderr, "%s: %s (errno = %d)\n", msg, strerror(errno), errno);
 	exit(-1);
 }
 
-void mount_fs(){
+void mount_fs()
+{
 	printf("Mounting filesystems\n");
 	// If /sys is not created, make it read-only (mode = 444)
 	if (mkdir("/sys", 0x124) && errno != EEXIST)
@@ -28,14 +35,50 @@ void mount_fs(){
 		panic("mount");
 }
 
-int main(){
-   pid_t pid = fork();
-   hello_world();
+int main()
+{
+	mount_fs();
+
+	printf("Forking to run programs");
+
+	for (int i = 0; i < len(programs); i++) {
+
+		const char *path = programs[i];
+		pid_t pid = fork();
+		
+      if (pid == -1) {
+			panic("fork");
+		} else if (pid) {
+			printf("Starting %s (pid = %d)\n", path, pid);
+		} else {
+			execl(path, path, (char *)NULL);
+			panic("execl");
+		}
+
+	}
+
    cfs();
 
-   for(;;){
-      sleep(1000);
-   }
-   
-   return 0;
+	int program_count = len(programs);
+
+	while (program_count) {
+
+		int wstatus;
+		pid_t pid = wait(&wstatus);
+		
+      if (WIFEXITED(wstatus))
+			printf("pid %d exited with %d\n", pid, WEXITSTATUS(wstatus));
+		else if (WIFSIGNALED(wstatus))
+			printf("pid %d killed by signal %d\n", pid, WTERMSIG(wstatus));
+		else
+			continue;
+		program_count--;
+
+	}
+
+
+	printf("init finished\n");
+	for (;;)
+		sleep(1000);
+	return 0;
 }
